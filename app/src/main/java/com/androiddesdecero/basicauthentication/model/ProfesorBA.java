714e8a8c466f5917312a6d27ec033ae1b24b95f6
Alberto <info@androiddesdecero.com>
2018-12-14 08:43:59 +0100

    06

---
+++ app/src/main/java/com/androiddesdecero/basicauthentication/model/ProfesorBA.java
@@ -0,0 +1,35 @@
+package com.androiddesdecero.basicauthentication.model;
+
+/**
+ * Created by albertopalomarrobledo on 14/12/18.
+ */
+
+public class ProfesorBA {
+    private Long id;
+    private String name;
+    private String salary;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+}
